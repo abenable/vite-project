fa1f1c8e5d71cfc52bfc0e1e7541499dd7ebe8f7
abenable <ableabenaitwe@gmail.com>
2024-03-02 21:08:48 +0300

    Add React Router and About page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import Layout from "./pages/layout/layout";
+import About from "./pages/about/about";
 
 const App = () => {
   return (
     <div className="App">
-      <Layout />{" "}
+      {/* <Layout />{" "} */}
+      <Routes>
+        <Route path="/" element={<Layout />} />
+        <Route path="about" element={<About />} />
+      </Routes>
     </div>
   );
 };
---
+++ src/pages/about/about.jsx
@@ -0,0 +1,24 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About hub-256</h1>
+      <p>Welcome to hub-256, the ultimate blog site for all things tech!</p>
+      <p>
+        Our mission is to provide you with the latest news, insights, and
+        tutorials on various topics such as web development, programming
+        languages, software engineering, and more.
+      </p>
+      <p>
+        Whether youre a beginner or an experienced developer, hub-256 is your
+        go-to resource for staying up-to-date with the ever-evolving tech
+        industry.
+      </p>
+      <p>
+        Stay tuned for exciting content and make sure to subscribe to our
+        newsletter to never miss an update!
+      </p>
+    </div>
+  );
+};
+
+export default About;
