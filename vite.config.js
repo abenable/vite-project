3d1f9bcdc0df5eac562b42455910fb88640a96d9
abenable <ableabenaitwe@gmail.com>
2024-03-03 00:53:53 +0300

    Remove unnecessary code in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import { resolve, dirname } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/vite-project/",
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(dirname(import.meta.url), "index.html"),
-        404: resolve(dirname(import.meta.url), "public/404.html"),
-      },
-    },
-  },
 });
